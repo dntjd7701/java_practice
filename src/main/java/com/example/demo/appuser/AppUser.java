0dd33b3abc12fff4631a04cedce7c0e0d5e9f055
dntjd7701 <dntjd7701@gmail.com>
2021-11-16 16:27:38 +0900

    #ERROR DB connect refused

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
 import java.util.Collection;
